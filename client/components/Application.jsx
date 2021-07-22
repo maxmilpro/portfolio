038529712194238383f682743078431ce8bee61c
Max Proano <proanom89@gmail.com>
2021-07-22 16:23:37 -0700

    Add initial Card layout to Application

--- client/components/Application.jsx
+++ client/components/Application.jsx
@@ -1,17 +1,17 @@
 import React from  'react';
+import { Card, Button } from 'react-bootstrap';
 
 const Application = ({ app }) => {
   const { title, description, technology, githubURL, image } = app;
   return (
-    <div>
-      <ul>
-        <li>{title}</li>
-        <li>{description}</li>
-        <li>{technology}</li>
-        <li>{githubURL}</li>
-        <li>{image}</li>
-      </ul>
-    </div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{description}</Card.Subtitle>
+          <Card.Text>{technology}</Card.Text>
+          <Card.Link href={githubURL}>GitHub Repo</Card.Link>
+        </Card.Body>
+      </Card>
   )
 }
 
