f9947084f2a7bad181e0ecbf738ecb9a005ebcfe
Max Proano <proanom89@gmail.com>
2021-07-22 16:34:31 -0700

    Center Application Cards

--- client/components/Application.jsx
+++ client/components/Application.jsx
@@ -4,7 +4,7 @@ import { Card, Button } from 'react-bootstrap';
 const Application = ({ app }) => {
   const { title, description, technology, githubURL, image } = app;
   return (
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '27rem', margin: 'auto' }}>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">{description}</Card.Subtitle>
